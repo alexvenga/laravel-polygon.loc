9b7c6bde2fa44de42ba1d54c3dd67ece2597404f
Alexey Bubnov <bubnov.alexey@gmail.com>
2020-09-05 03:34:57 +0300

    24.1

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -105,7 +105,9 @@ class CategoryController extends BaseController
             $data['slug'] = str_slug($data['title']);
         }
 
-        $result = $item->fill($data)->save();
+        //$result = $item->fill($data)->save();
+        // OR
+        $result = $item->update($data);
 
         if ($result) {
             return redirect()
